dd0615d316973ec76959025c8f98c2208ebd8103
Ramana Nagasamudram <rnagasam@stevens.edu>
2018-12-17 02:57:46 -0500

    Refactor execute function.
    Additionally, properly close file descriptors.

--- execute.c
+++ execute.c
@@ -16,7 +16,7 @@
 #define FOUT     1
 #define RD_FLAGS O_RDONLY
 #define WR_FLAGS O_WRONLY | O_TRUNC | O_CREAT
-#define AP_FLAGS O_WRONLY | O_APPEND
+#define AP_FLAGS O_WRONLY | O_CREAT | O_APPEND
 #define DEFAULT_MODE 0644
 
 extern int last_status;
@@ -33,95 +33,108 @@ ncommands(struct sish_command *comm)
 }
 
 int
-sish_execute(struct sish_command *comm)
+sish_execute(struct sish_command *cmd)
 {
-    int i, nc, status, prevfd, fd;
-    int pfds[2];
+    int i, nc, status, prevfd, fd, ex;
+    int p[2];
     pid_t pid;
     char *filename;
-    struct sish_command *tmp, *prev;
+    struct sish_command *curr, *prev;
 
     status = last_status;
-    nc = ncommands(comm);
+    nc = ncommands(cmd);
     fd = -1;
+    ex = 1;
 
     prevfd = STDIN_FILENO;
 
-    print_command(comm);
+    for (i = 0, curr = cmd, prev = NULL; i < nc;
+	 i++, prev = curr, curr = curr->next) {
 
-    for (i = 0, tmp = comm, prev = NULL; i < nc;
-	 i++, prev = tmp, tmp = tmp->next) {
-	
-	if (pipe(pfds) < 0)
+	if (pipe(p) < 0)
 	    err(127, "pipe");
+
+	if (prev)
+	    switch (prev->conn) {
+	    case IN: case OUT: case APPEND:
+		ex = 0;
+		break;
+	    default:
+		ex = 1;
+		break;
+	    }
 
 	if ((pid = fork()) < 0)
 	    err(127, "fork");
 
 	if (pid == 0) { /* child */
+
 	    if (dup2(prevfd, STDIN_FILENO) != STDIN_FILENO)
 		err(127, "dup2 stdin");
 
-	    if (tmp->next != NULL)
-		filename = tmp->next->command;
-
-	    switch (tmp->conn) {
-	    case PIPE:
-		if (dup2(pfds[FOUT], STDOUT_FILENO) != STDOUT_FILENO)
+	    switch (curr->conn) {
+	    case PIPE:		
+		if (dup2(p[FOUT], STDOUT_FILENO) != STDOUT_FILENO)
 		    err(127, "dup2 stdout");
-		/* FALLTHROUGH */
-		
-	    case OUT:
-		close(pfds[FIN]);
-		if ((fd = open(filename, WR_FLAGS, DEFAULT_MODE)) == -1)
-		    err(127, "open");
 
-		if (dup2(fd, STDOUT_FILENO) != STDOUT_FILENO)
-		    err(127, "dup2 stdout");
 		break;
 
 	    case IN:
-		close(pfds[FOUT]);
+		filename = curr->next->command;
 		if ((fd = open(filename, RD_FLAGS)) == -1)
 		    err(127, "open");
 
 		if (dup2(fd, STDIN_FILENO) != STDIN_FILENO)
 		    err(127, "dup2 stdin");
+
 		break;
+
+	    case OUT:
+		filename = curr->next->command;
+		if ((fd = open(filename, WR_FLAGS, DEFAULT_MODE)) == -1)
+		    err(127, "open");
+
+		(void)close(p[FOUT]); /* close write end -- set to fd */
 		
+		if (dup2(fd, STDOUT_FILENO) != STDOUT_FILENO)
+		    err(127, "dup2 stdout");
+
+		break;
+
 	    case APPEND:
-		close(pfds[FIN]);
+		filename = curr->next->command;
 		if ((fd = open(filename, AP_FLAGS, DEFAULT_MODE)) == -1)
 		    err(127, "open");
 
+		(void)close(p[FOUT]); /* close write end -- set to fd */
+
 		if (dup2(fd, STDOUT_FILENO) != STDOUT_FILENO)
 		    err(127, "dup2 stdout");
+
 		break;
 
 	    default:
-		close(pfds[FIN]);
 		break;
 	    }
 
-	    if (prev && (prev->conn == IN || prev->conn == OUT
-			 || prev->conn == APPEND)) {
-		printf("prev->command: %s\n", prev->command);
-		exit(EXIT_SUCCESS);
-	    }
+	    (void)close(p[FIN]); /* close read end */
 
-	    status = execvp(tmp->command, tmp->argv);
-	    fprintf(stderr, "exec: %s: %s\n", tmp->command, strerror(errno));
+	    if (!ex) exit(0); /* curr->command refers to a file */
+	    
+	    status = execvp(curr->command, curr->argv);
+	    fprintf(stderr, "%s: %s\n", curr->command, strerror(errno));
 	    exit(127);
 	} else { /* parent */
-	    
+
 	    if (waitpid(pid, &status, 0) < 0)
 		err(127, "waitpid");
 
-	    close(fd);
-	    close(pfds[FOUT]);
-	    prevfd = pfds[FIN];
+	    (void)close(p[FOUT]); /* close write end */
+	    prevfd = p[FIN];      /* save write end of child */
+
 	}
     }
 
+    (void)close(prevfd);
     return status;
 }
